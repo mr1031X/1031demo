321ed5ded177ab93f204ff13930c262d3b5549cd
mr1031X <chris@deferally.ai>
2024-02-15 17:03:16 -0600

    hid demo links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -14,7 +14,7 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  //{ name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'My Exchanges',
     href: '/dashboard/exchanges',
@@ -22,12 +22,12 @@ const links = [
   },
   { name: 'Chat', href: '/dashboard/chat', icon: ChatBubbleLeftEllipsisIcon },
   { name: 'Profile', href: '/dashboard/profile', icon: UserCircleIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  // {
+  //   name: 'Invoices',
+  //   href: '/dashboard/invoices',
+  //   icon: DocumentDuplicateIcon,
+  // },
+  // { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -8,7 +8,7 @@ export default function SideNav() {
     <div className="flex h-full flex-col px-3 md:px-2 bg-gray-800">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md p-4 md:h-40"
-        href="/"
+        href="/dashboard/exchanges"
       >
         <div className="w-32 text-white md:w-40">
           <DeferAllyLogo />
